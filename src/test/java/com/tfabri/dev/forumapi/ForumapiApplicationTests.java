a291869b77382206aa3f8c9b87a30cae21a832a5
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-17 19:13:35 -0300

    commit inicial: estrutura do projeto Spring Boot

---
+++ src/test/java/com/tfabri/dev/forumapi/ForumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfabri.dev.forumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
